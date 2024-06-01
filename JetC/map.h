5db5aecc52e181d828bb783e3ad7f2b4624a13f4
MainRacoon <kraev.egorka@gmail.com>
2024-06-01 19:06:32 +0200

    map

--- JetC/map.h
+++ JetC/map.h
@@ -4,11 +4,27 @@
 #include <stdlib.h>
 #include "./list.h"
 
+typedef struct{
+    jetlist keys;
+    jetlist values;
+    size_t len;
+}jetmap;
 
-
-
-
-
+jetmap jetMap(){
+    return (jetmap){jetList(),jetList(),0};
+}
+void jetMapAppend(jetmap*map,jetelement key, jetelement value){
+    jetListAppend(&map->keys,key);
+    jetListAppend(&map->values,value);
+    map->len++;
+}
+jetmap jetMapMerge(jetmap map1 , jetmap map2){
+    jetmap m = jetMap();
+    m.keys = jetListMerge(map1.keys, map2.keys);
+    m.keys = jetListMerge(map1.keys, map2.keys);
+    m.len = map1.len+map2.len;
+    return m;
+}
 
 
 
